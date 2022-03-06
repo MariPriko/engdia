5fdde20fdd7476f912313c0aa535da8b54ffb42c
MariPriko <mari.priko@gmail.com>
2022-03-06 18:49:07 +0300

    add responsive, modal, form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,61 @@
+$(document).ready(function () {
+    'use strict';
+
+// Fixed header
+    function stickHeader() {
+        let pos = $(".header").offset();
+        if (pos.top > 50) {
+            $(".header").addClass("sticky-header");
+        } else {
+            $(".header").removeClass("sticky-header");
+        }
+    }
+    stickHeader();
+    $(window).on("scroll", function () {
+        stickHeader()
+    });
+
+// Burger menu
+    $('.burger-menu__icon-open').click(function() {
+        $(this).siblings(".burger-menu__toggle-menu").addClass("show");
+        $(".burger-theme-style").fadeIn(200);
+        $("body").addClass("no-scroll");
+    });
+    $('.burger-close').click(function() {
+        $(".burger-menu__toggle-menu").removeClass("show");
+        $(".burger-theme-style").fadeOut(200);
+        $("body").removeClass("no-scroll");
+    });
+    $('.burger-link').click(function() {
+        $(".burger-menu__toggle-menu").removeClass("show");
+        $(".burger-theme-style").fadeOut(200);
+        $("body").removeClass("no-scroll");
+    });
+    $('.burger-theme-style').click(function() {
+        $(".burger-menu__toggle-menu").removeClass("show");
+        $(this).fadeOut(200);
+        $("body").removeClass("no-scroll");
+    });
+
+// Smooth links
+    $(".smooth").on("click", function (event) {
+        var id  = $(this).attr("href"),
+            top = $(id).offset().top - 80;
+        $("html, body").animate({scrollTop: top}, 600);
+    });
+
+// Input effect
+    $(".effect-input").val("");
+    $(".input-effect .effect-input").focusout(function(){
+        if($(this).val() != ""){
+            $(this).addClass("has-content");
+        }else{
+            $(this).removeClass("has-content");
+        }
+    });
+});
+
+// Config sliders
 const swiperServices = new Swiper('.swiper-services', {
     // Optional
     direction: 'horizontal',
@@ -10,9 +68,33 @@ const swiperServices = new Swiper('.swiper-services', {
     },
     // Responsive
     breakpoints: {
+        1930: {
+            slidesPerView: 4,
+            spaceBetween: 60
+        },
         1440: {
             slidesPerView: 3.5,
             spaceBetween: 80
+        },
+        1210: {
+            slidesPerView: 3.5,
+            spaceBetween: 50
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 15
         }
     }
 });
@@ -29,9 +111,33 @@ const swiperStep = new Swiper('.swiper-step', {
     },
     // Responsive
     breakpoints: {
+        1930: {
+            slidesPerView: 4,
+            spaceBetween: 60
+        },
         1440: {
             slidesPerView: 4.8,
             spaceBetween: 30
+        },
+        1210: {
+            slidesPerView: 3.8,
+            spaceBetween: 50
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 15
         }
     }
 });
@@ -51,12 +157,18 @@ const swiperStafDocs = new Swiper('.swiper-staffing-docs', {
         1440: {
             slidesPerView: 5,
             spaceBetween: 10
+        },
+        576: {
+            slidesPerView: 3,
+            spaceBetween: 10
+        },
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 10
         }
     }
 });
 swiperServices.params.speed = 1000;
+swiperStep.params.speed = 1000;
+swiperStafDocs.params.speed = 1000;
 
-
-$(document).ready(function () {
-
-});
